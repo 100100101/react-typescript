99fa4942c3717e47bf0481195df59405b541cb09
JSerGit <bibik.serej@gmail.com>
2021-01-02 22:05:24 +0300

    first commit

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+export const Navbar: React.FunctionComponent = () => (
+  <nav>
+    <div className="nav-wrapper deep-purple darken-1 px1">
+      <a href="/" className="brand-logo">
+        React + TypeScript
+      </a>
+      <ul className="right hide-on-med-and-down">
+        <li>
+          <a href="/">Список дел</a>
+        </li>
+        <li>
+          <a href="/">Информация</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+)
